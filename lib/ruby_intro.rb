c3383189dce41c0becb80f387911f2e599784858
Vivek Vamsi Inala <vivekvamsi@Viveks-MacBook-Air.local>
2022-09-10 22:59:14 -0500

    Add Implementation and Log file

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,34 +3,84 @@
 # Part 1
 
 def sum(arr)
-  # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum(arr)
-  # YOUR CODE HERE
+  if arr.size ==0
+    return 0
+  elsif arr.size==1
+    return arr.at(0)
+  end
+  first,second = arr.max(2)
+  return first + second
 end
 
 def sum_to_n?(arr, number)
-  # YOUR CODE HERE
+  hashSet = Set.new
+  arr.each{ |num|
+    if hashSet.include?(number-num)
+      return true
+    else
+      hashSet.add num
+    end
+  }
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant?(string)
-  # YOUR CODE HERE
+  if string.start_with?('A','E','I','O','U','a','e','i','o','u') || string.size==0
+    return false
+  elsif string[0].match?(/[[:alpha:]]/) 
+    return true
+  end
 end
 
 def binary_multiple_of_4?(string)
-  # YOUR CODE HERE
+  if string.size == 0 or not string !~ /[^01]/
+    return false
+  elsif (string.to_i(2))%4 == 0
+    return true
+  end
+  return false
 end
 
 # Part 3
 
 # Object representing a book
 class BookInStock
-  # YOUR CODE HERE
+  def initialize(isbn, price)
+    if isbn.empty? || (price <= 0)
+      raise ArgumentError.new("Please enter valid arguments- ISBN cannot be empty and Price should be greater than or equal to 0")
+    end
+    @isbn = isbn
+    @price = price
+  end
+
+  def isbn
+    @isbn
+  end
+
+  def price
+    @price
+  end
+
+  def isbn=(isbn)
+    @isbn = isbn
+  end
+
+  def price=(price)
+    @price = price
+  end
+
+  def price_as_string
+    formattedstr = format("%.2f",@price)
+    return "$" + formattedstr
+  end
 end
